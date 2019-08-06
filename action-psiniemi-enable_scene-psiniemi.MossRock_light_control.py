2cf2a4ef451b46b44d9a829c28c559f48dd3e7bb
Pasi Niemi <pasi.niemi@nitor.com>
2019-08-06 10:35:45 +0300

    Refactoring

--- action-psiniemi-enable_scene-psiniemi.MossRock_light_control.py
+++ action-psiniemi-enable_scene-psiniemi.MossRock_light_control.py
@@ -1,40 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import configparser
 from hermes_python.hermes import Hermes
 from hermes_python.ffi.utils import MqttOptions
 from hermes_python.ontology import *
-import io
 import mossrock as mr
 
-CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
 
-class SnipsConfigParser(configparser.SafeConfigParser):
-    def to_dict(self):
-        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
-
-
-def read_configuration_file(configuration_file):
-    try:
-        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
-            conf_parser = SnipsConfigParser()
-            conf_parser.readfp(f)
-            return conf_parser.to_dict()
-    except (IOError, configparser.Error) as e:
-        return dict()
-
 def subscribe_intent_callback(hermes, intentMessage):
-    conf = read_configuration_file(CONFIG_INI)
+    conf = mr.read_configuration_file(CONFIG_INI)
     action_wrapper(hermes, intentMessage, conf)
-
 
 def action_wrapper(hermes, intentMessage, conf):
     resp_msg = mr.send_scene_command(mr.get_scene_name(intentMessage))
     hermes.publish_end_session(intentMessage.session_id, resp_msg)
-    
-
 
 if __name__ == "__main__":
     mqtt_opts = MqttOptions()
--- mossrock.py
+++ mossrock.py
@@ -1,4 +1,21 @@
+import configparser
 import http.client
+import io
+
+CONFIGURATION_ENCODING_FORMAT = "utf-8"
+
+class SnipsConfigParser(configparser.SafeConfigParser):
+    def to_dict(self):
+        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
+
+def read_configuration_file(configuration_file):
+    try:
+        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
+            conf_parser = SnipsConfigParser()
+            conf_parser.readfp(f)
+            return conf_parser.to_dict()
+    except (IOError, configparser.Error) as e:
+        return dict()
 
 def get_light_name(intentMessage):
     if intentMessage.slots.light_name.first().value:
